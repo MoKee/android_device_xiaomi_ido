2f7d2c0bb6ec62aa39e11b28325d1051c7f4b42c
Michael Bestas <mkbestas@lineageos.org>
2018-03-19 04:15:55 +0200

    QCamera2: Drop usage of USE_JB_MR1
    Change-Id: I61142c9cdfbabe5c6c436562af94620191e2ed39

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -26,10 +26,6 @@ LOCAL_CFLAGS += -DUSE_MEDIA_EXTENSIONS
 #Debug logs are enabled
 #LOCAL_CFLAGS += -DDISABLE_DEBUG_LOG
 
-ifneq ($(call is-platform-sdk-version-at-least,18),true)
-LOCAL_CFLAGS += -DUSE_JB_MR1
-endif
-
 LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/../stack/common \
         frameworks/native/include/media/openmax \
